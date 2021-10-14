9201ace58b58704f32666702430f2a61063c2af7
5n0weirdo <5n0weirdo@protonmail.com>
2021-10-14 16:09:56 +0200

    Added Button

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Button from './Button'
 
 const Greet = () => {
     return (
         <div>
             <h1>Hello World</h1>
             <h3>Welcome to ReactJS</h3>
+            <Button />
         </div>
     )
 }
