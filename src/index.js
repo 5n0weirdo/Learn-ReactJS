fefeca2a486dc2ce90d7dd3fed98398bf036cc94
5n0weirdo <snoweirdo@protonmail.com>
2021-10-14 19:09:30 +0200

    Added className

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
+import react from "react";
 import React from "react";
 import ReactDOM from "react-dom";
 
 function Greet() {
   return (
-    <>
-      <h4>Hello World!</h4>
-    </>
-  )
+    <div className="">
+      <h1>Hello World!</h1>
+      <ul>
+        <li>
+          <a href="#">Hello World</a>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 ReactDOM.render(<Greet />, document.getElementById("root"));
