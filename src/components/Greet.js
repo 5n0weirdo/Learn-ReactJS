daeb09018e5dcffec149ea03d630b382d9eee8c2
5n0weirdo <5n0weirdo@protonmail.com>
2021-10-14 16:05:20 +0200

    Hello World

---
+++ src/components/Greet.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Greet = () => {
+    return (
+        <div>
+            <h1>Hello World</h1>
+            <h3>Welcome to ReactJS</h3>
+        </div>
+    )
+}
+
+export default Greet
